958629652c1233717397f22f5563a577a6967b14
LuccaPasqua <luccapasqua@gmail.com>
2022-05-22 13:40:21 -0300

    meu primeiro commit

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,29 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size='4xl'>
+      <ModalOverlay />
+      <ModalContent 
+        marginX="aut"
+        w="auto"
+        h="auto"
+        maxWidth={['300px', '500px', '900px']}
+        maxH={['350px', '450', '600px']}
+      >
+        <ModalBody p="0">
+          <Image
+            src={imgUrl}
+            maxWidth={['300px', '500px', '900px']}
+            maxH={['350px', '450', '600px']}
+          />
+        </ModalBody>
+        <ModalFooter bg='pGray.800'  h='2rem' px='20px' borderBottomRadius="5px" >
+          <Link href={imgUrl} isExternal fontSize="1rem" mr="auto">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent >
+    </Modal>
+  )
 }
